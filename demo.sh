c9214ba2da65ebe4519cffd73652537cfe18166b
qilei123 <qileimail123@gmail.com>
2022-01-29 02:12:41 -0500

    fds

---
+++ demo.sh
@@ -0,0 +1,1 @@
+python tools/demo.py --config experiments/siammask_r50_l3/config.yaml --snapshot experiments/siammask_r50_l3/model.pth --video "/home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0078.MOV"
