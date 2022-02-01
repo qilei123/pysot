832c3a6abb52669f071a5e9b4997d21311a3673b
qilei123 <qileimail123@gmail.com>
2022-02-01 16:48:06 -0500

    test

--- demo.sh
+++ demo.sh
@@ -1,2 +1,2 @@
-python tools/demo.py --config experiments/siammask_r50_l3/config.yaml --snapshot experiments/siammask_r50_l3/model.pth --video "/home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0078.MOV"
-#python tools/demo.py --config experiments/siamrpn_mobilev2_l234_dwxcorr/config.yaml --snapshot experiments/siamrpn_mobilev2_l234_dwxcorr/model.pth --video "/home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0078.MOV"
+#python tools/demo.py --config experiments/siammask_r50_l3/config.yaml --snapshot experiments/siammask_r50_l3/model.pth --video "/home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0078.MOV"
+python tools/demo.py --config experiments/siamrpn_mobilev2_l234_dwxcorr/config.yaml --snapshot experiments/siamrpn_mobilev2_l234_dwxcorr/model.pth --video "/home/qilei/DATASETS/trans_drone/trans_drone_videos2/DJI_0078.MOV"
